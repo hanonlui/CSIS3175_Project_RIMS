fe5779380604f7fad6786d8a6c6e6eea9d1fba3e
hanon <hanon.mobile@gmail.com>
2024-10-10 22:30:38 -0700

    adding login function firebase auth

--- app/src/main/java/com/example/csis3175_project_rims/functionPanel.java
+++ app/src/main/java/com/example/csis3175_project_rims/functionPanel.java
@@ -1,6 +1,11 @@
 package com.example.csis3175_project_rims;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +13,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class functionPanel extends AppCompatActivity {
 
+    FirebaseAuth auth;
+    TextView txtUserInfo;
+    Button btnLogout, btnDashboard, btnStocktaking, btnSKUManage;
+    FirebaseUser user;
+
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +34,32 @@ public class functionPanel extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        auth = FirebaseAuth.getInstance();
+        txtUserInfo = findViewById(R.id.txtUserInfo);
+        btnLogout = findViewById(R.id.btnLogout);
+        btnDashboard = findViewById(R.id.btnDashboard);
+        btnStocktaking = findViewById(R.id.btnStocktaking);
+        btnSKUManage = findViewById(R.id.btnSKUManage);
+        user = auth.getCurrentUser();
+
+        if(user == null){
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            finish();
+        } else {
+            txtUserInfo.setText("User ID: "+user.getEmail());
+        }
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
 }
